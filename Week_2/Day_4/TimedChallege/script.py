e53749d79c119ed030c464e2bd3e605b67502473
Dekel <dekelmaz09@gmail.com>
2023-03-29 17:17:59 +0300

    week 2 day 4 timed challenge

---
+++ Week_2/Day_4/TimedChallege/script.py
@@ -0,0 +1,15 @@
+
+# Count occurence
+
+# Write a program which takes a string and a character as an input, and finds out the number of occurrences the character has in the string.
+
+# String: Programming is cool!
+# Character: o
+# 3
+
+
+# String: This is a great example
+# Character: y
+# 0
+char_num = lambda string , char : string.count(char)
+print(char_num('helloooooooooooooooooooooo', 'o'))
