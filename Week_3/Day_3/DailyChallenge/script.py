4775d693c573d34d3836c9de83d20db96d5bd549
Hanippa <dekelmaz09@gmail.com>
2023-04-04 19:47:10 +0300

    w3d3 daily challenge

--- Week_3/Day_3/DailyChallenge/script.py
+++ Week_3/Day_3/DailyChallenge/script.py
@@ -22,7 +22,32 @@ class Circle:
             self.radius = diameter/2
     def area(self):
         return 3.14*self.radius*self.radius
+    def __str__(self):
+        string = ""
+        for i in range(1,4+1):
+            string += "\n"
+            string += "  "*(4-i)
+            for j in range(4):
+                string += "*"*i
+        for i in range(4,0,-1):
+            string += "\n"
+            string += "  "*(4-i)
+            for j in range(4):
+                string += "*"*i
+        return string + f"A circle with a radius of {self.radius}"
+    def __repr__(self):
+        return str(self.radius)
+    def __add__(self,circle):
+        return Circle(circle.radius+self.radius)
+    def __eq__(self, o):
+        return o.radius == self.radius
+    def __gt__(self,o):
+        return self.radius > o.radius
+    def __lt__(self,o):
+        return self.radius < o.radius
 
-circle = Circle(radius=10)
-
-print(circle.area())
+circle = Circle(radius=4)
+circle2 = Circle(diameter=10)
+print(circle>circle2)   
+cirlist = [circle,circle2]
+print(cirlist)
