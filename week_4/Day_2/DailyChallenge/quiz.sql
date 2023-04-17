b6848a64ca7fe9a006d2497ba876f88cae3e5233
Dekel <dekelmaz09@gmail.com>
2023-04-17 15:06:01 +0300

    w4d2 daily challenge

---
+++ week_4/Day_2/DailyChallenge/quiz.sql
@@ -0,0 +1,24 @@
+-- Questions
+
+-- Q1. What will be the OUTPUT of the following statement?   /// 0
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL )
+
+
+-- Q2. What will be the OUTPUT of the following statement?  // 2
+
+--     SELECT COUNT(*)
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+
+
+-- Q3. What will be the OUTPUT of the following statement?   // 2 real 0
+
+--     SELECT COUNT(*)
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
+
+
+-- Q4. What will be the OUTPUT of the following statement?  // 2
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL ) 
