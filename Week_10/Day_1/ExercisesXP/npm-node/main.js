b6573a1b7fe66e766101b9b1bc6719511056672a
Dekel <dekelmaz09@gmail.com>
2023-06-04 12:28:50 +0300

    w10d1 Exercises XP + Daily Challenge

---
+++ Week_10/Day_1/ExercisesXP/npm-node/main.js
@@ -0,0 +1,42 @@
+// Exercise 1 : Hello Node.js
+
+//     Create a new folder called - npm-node
+//     Create a JS file called - main.js
+
+//     Add the following code to the file main.js :
+
+//     const a = 5;
+//     const b = 10;
+
+
+//     Write some more code in the main.js file so when you run it in the command prompt it logs Hello you are 15
+
+
+// Exercise 2 : Hello Node.js #2
+
+//     Create a package.json file - use npm init
+//     Run the main.js file using npm start.
+//     Tip: you need to change the package.json file in order to achieve this.
+
+// You should output something in the command prompt, for example: Hi! How are you ?
+
+
+const i = 5;
+const d = 10;
+
+console.log(`hello you are ${i + d}`)
+
+// Exercise 3 : Nodemon
+
+//     Create a package.json file - use npm init
+//     Install nodemon
+
+//     Write the following code, in a main.js file:
+
+const a = 5;
+const b = 10;
+let x;
+const c = 6;
+x = a + b;
+console.log(x)
+console.log(x + c)
