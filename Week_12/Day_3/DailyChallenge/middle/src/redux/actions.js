0d555238ea72f37eab31e3b72ebe201e377745d9
Hanippa <dekelmaz09@gmail.com>
2023-06-20 20:55:15 +0300

    w12d3

--- Week_12/Day_3/DailyChallenge/middle/src/redux/actions.js
+++
@@ -1,7 +0,0 @@
-export const getUsers = (inputSearch) => (dispatch) => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data=> {
-    dispatch({type:'USERS', payload:data, inputSearch})
-    })
-}
